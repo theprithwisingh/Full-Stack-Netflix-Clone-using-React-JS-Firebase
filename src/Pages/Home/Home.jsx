b3560e7925e319de5db2b3038d38136fd29f4450
theprithwisingh <prithwisingh77@gmail.com>
2024-05-02 23:43:56 +0530

    titleCard

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../Components/TitleCards/TitleCards'
 function Home() {
   return (
     <div className='home'>
@@ -22,6 +23,7 @@ function Home() {
             <img src={info_icon} alt="" />
           More info</button>
         </div>
+        <TitleCards/>
       </div>
      </div>
     </div>
