70971e84544052dd1ff37a8e3b6f572178385293
theprithwisingh <prithwisingh77@gmail.com>
2024-05-04 00:44:56 +0530

    login pages

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -3,34 +3,35 @@ import './Login.css'
 import logo from '../../assets/logo.png'
 
 function Login() {
-    const[signState, setSignState] = useState('Sign In')
-  return (
-    <div className='login'>
-      <img src={logo} className='login-logo' alt="" />
-      <div className='login-form'>
-       <h1>{signState}</h1>
-        <form>
-            {signState==="Sing Up"?<input type="text" placeholder='Your name' />:<></>}
-            <input type="email" placeholder='Email' />
-            <input type="password" placeholder='Enter your password'/>
-            <button>{signState}</button>
-            <div className='form-help'>
-             <div className='remember'>
-                <input type="checkbox" name="" id="" />
-                <label htmlFor="">Remember Me</label>
-             </div>
-             <p>Need Help</p>
+    const [signState, setSignState] = useState('Sign In')
+    return (
+        <div className='login'>
+            <img src={logo} className='login-logo'/>
+            <div className='login-form'>
+                <h1>{signState}</h1>
+                <form>
+                    {signState==="Sign Up" ? <input type="text" placeholder='Your name' /> : null}
+                    <input type="email" placeholder='Email'/>
+                    <input type="password" placeholder='Enter your password'/>
+                    <button>{signState}</button>
+                    <div className='form-help'>
+                        <div className='remember'>
+                            <input type="checkbox"/>
+                            <label htmlFor="">Remember Me</label>
+                        </div>
+                        <p>Need Help</p>
+                    </div>
+                </form>
+                <div className='form-switch'>
+                    {signState==="Sign In" ?
+                        <p>New to Netflix?<span onClick={() => {setSignState("Sign Up")}}>Sign Up Now</span></p>
+                        :
+                        <p>Already have account? <span onClick={() => {setSignState("Sign In")}}>Sign In Now</span></p>
+                    }
+                </div>
             </div>
-        </form>
-        <div className='form-switch'>
-            {signState==="Sign In"?
-            <p>New to Netflix?<span onClick={()=>{setSignState("Sign Up")}}>Sign Up Now</span></p>
-            :<p>Already have account? <span onClick={()=>{setSignState("Sign In")}}>Sign In Now</span></p>
-            }
         </div>
-    </div>
-</div>
-  )
+    )
 }
 
-export default Login;
+export default Login
